9e650007dadc609aaaa3b9d28818174b1665d07d
Andrew McDermott <aim@frobware.com>
2022-09-07 10:28:42 +0100

    main: include <ncurses.h>

--- main.c
+++ main.c
@@ -8,8 +8,8 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include <curses.h>
 #include <libusb.h>
+#include <ncurses.h>
 
 // The LG vendor id is used to distinguish LG devices from
 // those of other vendors.
